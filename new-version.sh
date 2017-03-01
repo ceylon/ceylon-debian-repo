9244b86be30a5001079a61c2b40e6f04952e8d37
Tako Schotanus <tako@ceylon-lang.org>
2017-03-01 19:03:54 +0100

    The `new-version.sh` script now determines the priority automatically to avoid mistakes

--- new-version.sh
+++ new-version.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-if [[ $# != 2 ]]; then
-    echo "Usage: $0 <version> <version-prio>"
+if [[ $# != 1 ]]; then
+    echo "Usage: $0 <version>"
     echo ""
-    echo "eg: $0 1.2.1 12010"
+    echo "eg: $0 1.2.1"
     exit
 fi
 
 VERSION=$1
-PRIO=$2
+
+VS=(${VERSION//\./ })
+if [[ ${#VS[@]} != 3 ]]; then
+    echo "Error: version must have 3 numbers separated by periods, eg 1.2.1"
+    exit
+fi
+
+PRIO=$(printf "%d%02d%02d0" ${VS[0]} ${VS[1]} ${VS[2]})
+
+echo "Using version $VERSION and priority $PRIO..."
+exit
 
 git checkout -q -b "${VERSION}"
 
