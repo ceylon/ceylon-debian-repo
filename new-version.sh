2d4abf472c6b312022ce9b3e7552bf776774a065
Stephane Epardaud <stef@epardaud.fr>
2017-04-27 14:40:56 +0200

    Prepare for automated releases

--- new-version.sh
+++ new-version.sh
@@ -19,6 +19,11 @@ PRIO=$(printf "%d%02d%02d0" ${VS[0]} ${VS[1]} ${VS[2]})
 
 echo "Using version $VERSION and priority $PRIO..."
 
+perl -pi -e 's/(#.*\@NEW_VERSION\@)/DEBS="\$DEBS ceylon-$VERSION_$VERSION-0_all.deb"\n$1/' repo/build.sh
+dch --changelog dist-pkg/debian/changelog --newversion ${VERSION}-0 --package ceylon-$VERSION "New release $VERSION"
+dch --changelog dist-pkg/debian/changelog --release Gee
+git commit -a -m "New version $VERSION"
+
 git checkout -q -b "${VERSION}"
 
 git mv dist-pkg/debian/ceylon-VERSION.postinst dist-pkg/debian/ceylon-${VERSION}.postinst 
--- repo/build.sh
+++ repo/build.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-DEBS="ceylon-0.1_0.1_all.deb \
- ceylon-0.2_0.2_all.deb \
- ceylon-0.3_0.3_all.deb \
- ceylon-0.3.1_0.3.1_all.deb \
- ceylon-0.4_0.4_all.deb \
- ceylon-0.5_0.5_all.deb \
- ceylon-0.6_0.6_all.deb \
- ceylon-1.0.0_1.0.0_all.deb \
- ceylon-1.1.0_1.1.0_all.deb \
- ceylon-1.2.0_1.2.0_all.deb \
- ceylon-1.2.1_1.2.1_all.deb \
- ceylon-1.2.2_1.2.2_all.deb \
- ceylon-1.3.0_1.3.0_all.deb \
- ceylon-1.3.1_1.3.1-1_all.deb \
- ceylon-1.3.2_1.3.2-0_all.deb"
+DEBS="ceylon-0.1_0.1_all.deb"
+DEBS="$DEBS ceylon-0.2_0.2_all.deb"
+DEBS="$DEBS ceylon-0.3_0.3_all.deb"
+DEBS="$DEBS ceylon-0.3.1_0.3.1_all.deb"
+DEBS="$DEBS ceylon-0.4_0.4_all.deb"
+DEBS="$DEBS ceylon-0.5_0.5_all.deb"
+DEBS="$DEBS ceylon-0.6_0.6_all.deb"
+DEBS="$DEBS ceylon-1.0.0_1.0.0_all.deb"
+DEBS="$DEBS ceylon-1.1.0_1.1.0_all.deb"
+DEBS="$DEBS ceylon-1.2.0_1.2.0_all.deb"
+DEBS="$DEBS ceylon-1.2.1_1.2.1_all.deb"
+DEBS="$DEBS ceylon-1.2.2_1.2.2_all.deb"
+DEBS="$DEBS ceylon-1.3.0_1.3.0_all.deb"
+DEBS="$DEBS ceylon-1.3.1_1.3.1-1_all.deb"
+DEBS="$DEBS ceylon-1.3.2_1.3.2-0_all.deb"
+# @NEW_VERSION@
+
 rm -rf db pool dists
 
 for deb in $DEBS
